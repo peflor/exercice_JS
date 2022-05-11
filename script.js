0108b269ed28b56491bccdc89169fb35b4a0155b
Florian <92161528+peflor@users.noreply.github.com>
2022-05-11 14:42:43 +0200

    Exercice a Tester

---
+++ script.js
@@ -0,0 +1,31 @@
+/*
+   EXERCICE 1 : Parcours d'un tableau
+   
+   Vous devez modifier en JavaScript le code de la 
+   fonction "exercice1()" ci-dessous, afin de parcourir
+   le tableau "tabNombres" avec une boucle "For", et retourner
+   une chaine de caractères affichant les informations suivantes :
+      - La plus petite valeur présente dans le tableau
+      - La plus grande valeur présente dans le tableau
+      - La somme des valeurs présentes dans le tableau
+      - La moyenne des valeurs présentes dans le tableau.
+      
+   Merci de ne pas utiliser les fonctions Math.min ni Math.max 
+   pour effectuer cet exercice.
+   
+   Par exemple, pour le tableau [45, 78, 37, 12, 25, 27]
+   L'output devrait afficher exactement :
+   Résultat exercice 1 : 
+      - Plus petit nombre : 12
+      - Plus grand nombre : 78
+      - Somme des nombres : 224
+      - Moyenne des nombres : 37
+*/
+
+function exercice1() {
+    var resultatsExercice = "Résultat exercice 1 : ";
+    var tabNombres = [45, 78, 37, 12, 25, 27];
+   
+   
+    // Saisir votre code ici
+} 
